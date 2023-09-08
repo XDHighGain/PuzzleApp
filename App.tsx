fbe42e3d307cff54da0b54583c50d336ef6ff1f2
Igor Savchenko <traceskype1@gmail.com>
2023-09-08 15:35:00 +0300

    ios fix

--- App.tsx
+++ App.tsx
@@ -6,32 +6,32 @@ import { AuthScreen, NavigationNameAuthScreen } from './src/UI/Screens/AuthScree
 import { MainScreen, NavigationNameMainScreen } from './src/UI/Screens/MainScreen';
 import { navigationRef } from './src/Navigation/RootOptions';
 import 'react-native-reanimated';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 export const isReadyRef2 = React.createRef<(() => void) | boolean | null>();
+
 function App() {
-
   return (
-
-    <NavigationContainer
-      ref={navigationRef}>
-      <SafeAreaView
-        edges={['top']}
+    <SafeAreaProvider>
+      <SafeAreaView edges={['top']}
         style={{ flex: 1 }}>
-        <StatusBar barStyle={Platform.OS === 'ios' ? 'dark-content' : 'default'} />
-        <Stack.Navigator initialRouteName={NavigationNameAuthScreen}>
-          <Stack.Screen
-            name={NavigationNameAuthScreen}
-            component={AuthScreen}
-            options={{ headerShown: false }} />
-          <Stack.Screen
-            name={NavigationNameMainScreen}
-            component={MainScreen}
-            options={{ headerShown: false }} />
-        </Stack.Navigator>
+        <NavigationContainer
+          ref={navigationRef}>
+          <StatusBar barStyle={Platform.OS === 'ios' ? 'dark-content' : 'default'} />
+          <Stack.Navigator initialRouteName={NavigationNameAuthScreen}>
+            <Stack.Screen
+              name={NavigationNameAuthScreen}
+              component={AuthScreen}
+              options={{ headerShown: false }} />
+            <Stack.Screen
+              name={NavigationNameMainScreen}
+              component={MainScreen}
+              options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
-    </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
